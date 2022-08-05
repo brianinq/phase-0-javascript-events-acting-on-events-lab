61f9959c2ed496575b9350e81bf7c46e9a07f948
brian <kipkoechbrian0@gmail.com>
2022-08-05 11:33:49 +0300

    complete assignment requirements

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+const dodger = document.querySelector("#dodger");
+dodger.style.background = "#ff69b4";
+dodger.style.bottom = "100px";
+dodger.style.left = "0px";
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft()
+  } else if(event.key === "ArrowRight"){
+    moveDodgerRight()
+  }
+});
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left;
+  const left = parseInt(leftNumbers, 10);
+
+//   if (left <0 ) {
+    dodger.style.left = `${left - 1}px`;
+//   }
+}
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 40 ) {
+    dodger.style.left = `${left + 1}px`;
+  }
+
+}
